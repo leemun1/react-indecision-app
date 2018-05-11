c054c6dfd7d5977f16330cfab83dc86e558d5d41
Mike <munkyu0813@gmail.com>
2018-05-11 14:58:26 -0400

    add modals and clean up stateless components

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-const Option = (props) => {
-  return (
-    <div>
-      {props.optionText}
-      <button
-        onClick={() => props.handleDeleteOption(props.optionText)}>
-        remove
+const Option = (props) => (
+  <div>
+    {props.optionText}
+    <button
+      onClick={() => props.handleDeleteOption(props.optionText)}>
+      remove
       </button>
-    </div>
-  );
-}
+  </div>
+);
 
 export default Option;
