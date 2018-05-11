cd069db75787a8467a67fef29cf7502d9fba1411
Mike <munkyu0813@gmail.com>
2018-05-11 03:48:06 -0400

    separate out components and setup webpack with babel

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('hello there.js is running');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+import AddOption from './components/AddOption';
+import Options from './components/Options';
+import Header from './components/Header';
+import Action from './components/Action';
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
