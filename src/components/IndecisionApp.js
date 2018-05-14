e65181bd0216898b2a87a599af8c84867719ef57
Mike <munkyu0813@gmail.com>
2018-05-13 23:59:24 -0400

    style application with scss

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -10,7 +10,7 @@ export default class IndecisionApp extends React.Component {
     options: [],
     selectedOption: undefined
   }
-  
+
   handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }));
   }
@@ -67,15 +67,19 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle} />
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick} />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption} />
-        <AddOption
-          handleAddOption={this.handleAddOption} />
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick} />
+          <div className="widget">
+            <Options
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption} />
+            <AddOption
+              handleAddOption={this.handleAddOption} />
+          </div>
+        </div>
         <OptionModal
           selectedOption={this.state.selectedOption}
           handleOkay={this.handleOkay} />
